4f369f94a2d0339662f24ea59c7c615efd808b55
Sujay Koujalgi <suk96126@gmail.com>
2023-10-31 16:24:20 -0500

    Code for head command in unix

---
+++ #33/head.py
@@ -0,0 +1,55 @@
+import sys
+import argparse
+
+#Main function to mimic head command
+def main(files, n, c):
+
+    #Check args has file name. If not then take in the 
+    #consle input
+    if not files:
+        while True:
+            print(input())
+        pass
+
+    #Loop through multiple files
+    for file in files:
+        count = 0
+        print(f"==> {file} <==")
+
+        #Conditional logic based on line or byte request
+
+        if n:
+            file_1 = open(file, 'r')
+            for i in range(int(n)):
+                line = file_1.readline()
+
+                #End if document ends before the n lines 
+                if not line:
+                    break
+                count += 1
+                print(line.strip())
+
+        elif c:
+            file_1 = open(file, 'rb')
+            file = file_1.read(int(c))
+            #Ignoring non ascii cahracters while printing byte stream
+            print(file.decode('ascii', errors = 'ignore'))
+
+        #If no flags, then print entire document
+        else:
+            while line := file_1.readline():
+
+                count += 1
+                print(line.strip())
+        
+        
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Displays the top n lines of text")
+    parser.add_argument('-n')
+    parser.add_argument('-c')
+    parser.add_argument('files', nargs="+")
+    args = parser.parse_args()
+    
+    main(**vars(args))
